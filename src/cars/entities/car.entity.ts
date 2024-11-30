606d4932628c98d92244c70089e68c2db0462b7d
will-developer <william_limaa@hotmail.com>
2024-11-30 10:58:31 -0300

    add: implement car.entity

--- src/cars/entities/car.entity.ts
+++ src/cars/entities/car.entity.ts
@@ -1,1 +1,10 @@
-export class Car {}
+import { Car } from '@prisma/client';
+
+export class CarEntity implements Car {
+  id: number;
+  brand: string;
+  model: string;
+  plate: string;
+  year: number;
+  created_at: Date;
+}
