09c5a8f888e8947dab1fe8f9206afef77418ca0b
will-developer <william_limaa@hotmail.com>
2024-11-30 12:25:09 -0300

    feat: add and configure prisma service

--- src/cars/cars.module.ts
+++ src/cars/cars.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CarsService } from './cars.service';
 import { CarsController } from './cars.controller';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Module({
-  controllers: [CarsController],
-  providers: [CarsService],
+  controllers: [CarsController, PrismaService],
+  providers: [CarsService, PrismaService],
 })
 export class CarsModule {}
